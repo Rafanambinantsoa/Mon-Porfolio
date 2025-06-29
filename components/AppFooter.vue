58d4cc4b568f0f46b0c7358805bd87dc406a190f
rafanambinantsoa <akutagawakarim@gmail.com>
2025-06-29 21:51:17 +0300

    ANother  preparation ( Heart Break Anniversaty by F   )

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,35 +1,38 @@
 <template>
     <footer class='flex md:justify-between border-top text-menu-text font-fira_retina'>
 
-        <!-- social icons -->
+        <!-- social icons kim tsukasa -->
         <div class="w-full flex justify-between md:justify-start">
             <span id="social-title" class="h-full flex justify-center items-center border-right px-5">
                 me trouver sur:
             </span>
             <div id="social-icons" class="flex">
-                <NuxtLink :to="social.instagram.url + social.instagram.user" target="_blank" class="flex justify-center items-center">
-                    <img src="/icons/social/instagram.svg"/>
+                <NuxtLink :to="social.instagram.url + social.instagram.user" target="_blank"
+                    class="flex justify-center items-center">
+                    <img src="/icons/social/instagram.svg" />
                 </NuxtLink>
-                <NuxtLink :to="social.facebook.url + social.facebook.user" target="_blank" class="flex justify-center items-center">
-                    <img src="/icons/social/facebook.svg"/>
+                <NuxtLink :to="social.facebook.url + social.facebook.user" target="_blank"
+                    class="flex justify-center items-center">
+                    <img src="/icons/social/facebook.svg" />
                 </NuxtLink>
-                <NuxtLink :to="social.github.url + social.github.user" target="_blank" class="flex md:hidden justify-center items-center">
-                <img src="/icons/social/github.svg"/>
-            </NuxtLink>
+                <NuxtLink :to="social.github.url + social.github.user" target="_blank"
+                    class="flex md:hidden justify-center items-center">
+                    <img src="/icons/social/github.svg" />
+                </NuxtLink>
             </div>
         </div>
 
         <!-- github user -->
-        <NuxtLink :to="social.github.url + social.github.user" target="_blank" class="hidden md:flex items-center px-5 border-left">
+        <NuxtLink :to="social.github.url + social.github.user" target="_blank"
+            class="hidden md:flex items-center px-5 border-left">
             @{{ social.github.user }}
-            <img src="/icons/social/github.svg"/>
+            <img src="/icons/social/github.svg" />
         </NuxtLink>
 
     </footer>
 </template>
 
 <style>
-
 footer {
     height: 40px;
     min-height: 40px;
@@ -40,26 +43,31 @@ footer a:hover {
     background-color: #1e2d3d74;
 }
 
-#social-icons > a {
+#social-icons>a {
     border-right: 1px solid #1E2D3D;
     height: 100%;
     width: 50px;
- }
+}
 
-#social-icons > a > img {
-    width: 1.25rem; /* 20px */
-    height: 1.25rem; /* 20px */
+#social-icons>a>img {
+    width: 1.25rem;
+    /* 20px */
+    height: 1.25rem;
+    /* 20px */
     margin: auto;
     opacity: 0.4;
 }
 
-footer > a > img {
-    width: 1.25rem; /* 20px */
-    height: 1.25rem; /* 20px */
-    margin-left: 0.5rem; /* 8px */
-  }
+footer>a>img {
+    width: 1.25rem;
+    /* 20px */
+    height: 1.25rem;
+    /* 20px */
+    margin-left: 0.5rem;
+    /* 8px */
+}
 
-#social-icons > a:hover img {
+#social-icons>a:hover img {
     opacity: 1;
 }
 
@@ -69,17 +77,18 @@ footer > a > img {
         border-right: none;
     }
 
-    #social-icons > a {
+    #social-icons>a {
         border-right: none;
         border-left: 1px solid #1E2D3D;
     }
 
-    #social-icons > a > img {
-        width: 1.5rem; /* 20px */
-        height: 1.5rem; /* 20px */
-  }
+    #social-icons>a>img {
+        width: 1.5rem;
+        /* 20px */
+        height: 1.5rem;
+        /* 20px */
+    }
 }
-
 </style>
 
 <script>
@@ -87,7 +96,7 @@ export default {
     name: 'AppFooter',
     data() {
         return {
-          route: this.$route.path,
+            route: this.$route.path,
         }
     },
     setup() {
